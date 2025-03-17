86861721241673d03bf5a99a7f721e86d4f02f97
Benjamin Ricchiuto <beni.ricchiuto@gmail.com>
2025-03-17 01:24:36 +0100

    Add logging initialization and hexdump functionalities to ChiakiLogWrapper

--- pybind/src/core/log.cpp
+++ pybind/src/core/log.cpp
@@ -65,12 +65,31 @@ void init_core_log(py::module &m)
 
     py::class_<ChiakiLogWrapper>(m, "ChiakiLog")
         .def(py::init<uint32_t, std::function<void(ChiakiLogLevel, std::string, void *)>, void *>(),
-             py::arg("level_mask") = CHIAKI_LOG_ALL & ~LogLevel.INFO.value,
+             py::arg("level_mask") = CHIAKI_LOG_ALL & ~CHIAKI_LOG_INFO,
              py::arg("cb") = nullptr,
              py::arg("user") = nullptr)
         .def_property("level_mask", &ChiakiLogWrapper::get_level_mask, &ChiakiLogWrapper::set_level_mask)
         .def_property("cb", &ChiakiLogWrapper::get_cb, &ChiakiLogWrapper::set_cb)
         .def_property("user", &ChiakiLogWrapper::get_user, &ChiakiLogWrapper::set_user);
+
+    m.def("chiaki_log_init", [](ChiakiLogWrapper &log, uint32_t level_mask, ChiakiLogCb cb, void *user)
+        {
+            chiaki_log_init(log.raw(), level_mask, cb, user);
+        },
+        py::arg("log"),
+        py::arg("level_mask") = CHIAKI_LOG_ALL & ~CHIAKI_LOG_INFO,
+        py::arg("cb") = nullptr,
+        py::arg("user") = nullptr,
+        "Initialize the log.");
+    
+    m.def("chiaki_log_set_level", [](ChiakiLogWrapper &log, uint32_t level_mask)
+        {
+            chiaki_log_set_level(log.raw(), level_mask);
+        },
+        py::arg("log"),
+        py::arg("level_mask"),
+        "Set the log level."
+    );
 
     m.def("chiaki_log_cb_print", &chiaki_log_cb_print,
         py::arg("level"),
@@ -79,13 +98,35 @@ void init_core_log(py::module &m)
         "Logs a message using chiaki_log_cb_print."
     );
 
-    m.def("chiaki_log", [](ChiakiLogWrapper &log, ChiakiLogLevel level, const std::string &fmt)
+    m.def("chiaki_log", [](ChiakiLogWrapper &log, ChiakiLogLevel log_level, const std::string &fmt)
         {
-            chiaki_log(log.raw(), level, fmt.c_str());
+            chiaki_log(log.raw(), log_level, "%s", fmt.c_str());
         }, 
         py::arg("log"),
         py::arg("level"),
         py::arg("fmt"),
         "Logs a message using chiaki_log."
+    );
+
+    m.def("chiaki_log_hexdump", [](ChiakiLogWrapper &log, ChiakiLogLevel log_level, const uint8_t *buf, size_t buf_size)
+        {
+            chiaki_log_hexdump(log.raw(), log_level, buf, buf_size);
+        },
+        py::arg("log"),
+        py::arg("level"),
+        py::arg("buf"),
+        py::arg("buf_size"),
+        "Logs a hexdump using chiaki_log_hexdump."
+    );
+
+    m.def("chiaki_log_hexdump_raw", [](ChiakiLogWrapper &log, ChiakiLogLevel log_level, const uint8_t *buf, size_t buf_size)
+        {
+            chiaki_log_hexdump_raw(log.raw(), log_level, buf, buf_size);
+        },
+        py::arg("log"),
+        py::arg("level"),
+        py::arg("buf"),
+        py::arg("buf_size"),
+        "Logs a hexdump using chiaki_log_hexdump_raw."
     );
 }
